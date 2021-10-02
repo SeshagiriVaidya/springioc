df37f6a70fc2e8bad6c89183f3af69746bde3ee7
Seshagiri Vaidya <vaidya.seshagiri@gmail.com>
2021-10-02 22:33:24 +0530

    commit of Mobile and jio file

--- src/com/selenium/ioc/Airtel.java
+++ src/com/selenium/ioc/Airtel.java
@@ -6,6 +6,7 @@ public class Airtel implements Sim{
     @Override
     public void calling() {
         System.out.println("Calling using Airtel Sim");
+        System.out.println("Inside the Airtel");
 
     }
 
--- src/com/selenium/ioc/Jio.java
+++ src/com/selenium/ioc/Jio.java
@@ -6,6 +6,7 @@ public class Jio implements Sim{
     @Override
     public void calling() {
         System.out.println("Calling using Jio Sim");
+        System.out.println("Again changed");
 
     }
 
